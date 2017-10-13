d9e81a142cfcdca7b8112d60b4291a474a19e718
Alan Russell <ajrussellaudio@gmail.com>
2017-10-13 14:46:19 +0100

    add word and reveal chars

---
+++ word.rb
@@ -0,0 +1,19 @@
+class Word
+
+  def initialize(hidden_word)
+    @hidden_word = hidden_word
+  end
+
+  def display(guesses = [])
+    displayed_word = ""
+    for char in @hidden_word.split('')
+      if guesses.include?(char)
+        displayed_word << char
+      else
+        displayed_word << "*"
+      end
+    end
+    return displayed_word
+  end
+
+end
